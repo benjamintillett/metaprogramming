6e7bd397ad2ec62ee06af93144731cb668f4e381
benjamintillett <benjamin.tillett@gmail.com>
2014-10-08 11:49:28 +0100

    add awards method a wrewite method missing to use metaprogramming to be able to respond to has_*? mathod calls

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,25 @@
 class Student
 
+	attr_accessor :awards
+
+	def initialize
+		@awards = []
+	end
+
 	def method_missing(name)
 		if name.to_s.match(/^has_\w+\?$/)
-			self.class.send(:define_method, name){ false }
+			self.class.send(:define_method, name) do 
+				return true if @awards.include? name[4..-2].to_sym
+				false
+			end
 			self.send(name)
 		else
 			super
 		end
+	end
+
+	def award(sticker)
+		 @awards << sticker 
 	end
 
 end
